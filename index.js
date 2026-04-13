fe2aa1bac4fb6ae9345da07b06c7eb4a82f6b1d6
Kong Rocxam <rocxamkong@gmail.com>
2026-04-13 19:37:19 +0300

    Add new routes for users, groups, sessions, and admin

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ app.get("/", (req, res) => {
 
 // Routes
 const authRoutes = require("./routes/auth");
+const groupRoutes = require("./routes/groups");
+const sessionRoutes = require("./routes/sessions");
+const userRoutes = require("./routes/users");
+const adminRoutes = require("./routes/admin");
 app.use("/api", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/groups", groupRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 // Start server
 app.listen(process.env.PORT, () => {
